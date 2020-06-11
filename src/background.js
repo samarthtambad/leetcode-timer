c187675d627cb3c86695bfa47b051e249c63a4ae
samarthtambad <tambad.samarth@gmail.com>
2020-06-10 23:43:55 -0400

    icon change, states

--- src/background.js
+++ src/background.js
@@ -1,1 +1,100 @@
 global.browser = require('webextension-polyfill');
+
+var current;
+var count_timer;
+var running = false;
+var sound = new Audio('sound.mp3');
+
+const states = {
+  READY: 'ready',
+  RUNNING: 'running',
+  BACKGROUND: 'background',
+};
+
+var state = states.BACKGROUND;
+
+function reset() {
+  current = 25;
+  clearTimeout(count_timer);
+  chrome.browserAction.setBadgeText({ text: '' });
+}
+
+function popNotification() {
+  var message_index = Math.round(Math.random() * (messages.length - 1));
+  var message = '"' + messages[message_index] + '"';
+  chrome.storage.sync.get({ reqClick: 'yes' }, function(items) {
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: 'Timer.png',
+      title: chrome.i18n.getMessage('timeUp'),
+      message: message,
+      contextMessage: new Date().toLocaleTimeString(), // in gray text
+      eventTime: Date.now(), // add a event time stamp
+      isClickable: true, // show hand pointer when hover
+      requireInteraction: items.reqClick === 'yes', // if true, do not close until click
+    });
+  });
+}
+
+// Send a request from the extension to a content script
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, { greeting: 'hello' }, function(response) {
+    console.log(response.farewell);
+  });
+});
+
+// Receive messages from content script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === 'stop_icon') {
+    alert('stop icon');
+    chrome.browserAction.setIcon({ path: 'icons/icon_stop_128.png' });
+    chrome.tabs.query({ active: true, windowType: 'normal', currentWindow: true }, function(d) {
+      var tabId = d[0].id;
+      chrome.browserAction.setIcon({ path: 'icons/icon_stop_128.png', tabId: tabId });
+    });
+  }
+  console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension');
+  if (request.greeting == 'hello') sendResponse({ farewell: 'goodbye' });
+});
+
+function start() {
+  chrome.browserAction.setIcon({ path: 'icons/icon_stop_128.png' });
+  state = states.RUNNING;
+}
+
+function stop() {
+  chrome.browserAction.setIcon({ path: 'icons/icon_play_128.png' });
+  state = states.STOPPED;
+}
+
+function iconClick() {
+  alert('click');
+  switch (state) {
+    case states.BACKGROUND:
+      break;
+    case states.READY:
+      start();
+      break;
+    case states.RUNNING:
+      stop();
+      break;
+  }
+}
+
+chrome.browserAction.onClicked.addListener(iconClick);
+chrome.notifications.onClicked.addListener(function(notificationid) {
+  chrome.notifications.clear(notificationid);
+});
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  alert('onActivated');
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    let url = tabs[0].url;
+    // use `url` here inside the callback because it's asynchronous!
+    alert(url);
+  });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabID, info, tab) {
+  alert('onUpdated');
+});
--- src/content.js
+++ src/content.js
@@ -1,2 +1,33 @@
 // console.log("content script running")
 // document.body.style.background = "black";
+alert('content script');
+
+// on current tab, change icon
+// chrome.runtime.sendMessage({action: "stop_icon"})
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    let url = tabs[0].url;
+    // use `url` here inside the callback because it's asynchronous!
+    alert(url);
+  });
+});
+
+// on icon click, establish
+
+// url matches with pattern for content_scripts
+chrome.runtime.onConnect.addListener(function(port) {
+  chrome.runtime.sendMessage({ greeting: 'hello' }, function(response) {
+    console.log(response.farewell);
+  });
+});
+
+// Send a request from a content script
+chrome.runtime.sendMessage({ greeting: 'hello' }, function(response) {
+  console.log(response.farewell);
+});
+
+// Receive message from background script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension');
+  if (request.greeting == 'hello') sendResponse({ farewell: 'goodbye' });
+});
